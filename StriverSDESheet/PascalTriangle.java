5a5b64e6db8ca2a038987739fa685625f727f310
souryadeep <165980585+souryeahdeep@users.noreply.github.com>
2025-08-23 20:39:00 +0530

    Update PascalTriangle.java

--- StriverSDESheet/PascalTriangle.java
+++ StriverSDESheet/PascalTriangle.java
@@ -1,5 +1,37 @@
 package StriverSDESheet;
 
+import java.util.*;
+
 public class PascalTriangle {
 
+    public static void main(String[] args) {
+        PascalTriangle ob = new PascalTriangle();
+        List<List<Integer>> sol = ob.getList(5);//n^2
+        System.out.println(sol);
+    }
+
+    private List<List<Integer>> getList(int n) {
+        List<List<Integer>> ls = new ArrayList<>();
+        List<Integer> sub = new ArrayList<>();
+        sub.add(1);
+        ls.add(sub);
+        if (n == 1) {
+            return ls;
+        }
+        //n-1
+        while (ls.size() < n) {
+            sub = new ArrayList<>();
+            //n
+            for (int i = 0; i <= ls.getLast().size(); i++) {
+                if (i == 0 || i == ls.getLast().size()) {
+                    sub.add(1);
+                } else {
+                    int temp = ls.getLast().get(i) + ls.getLast().get(i - 1);
+                    sub.add(temp);
+                }
+            }
+            ls.add(sub);
+        }
+        return ls;
+    }
 }
